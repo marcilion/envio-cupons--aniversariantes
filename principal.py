684a19190f83ee9ef4bf3c513afb8ba829d97377
unknown <marcilio@example.com>
2023-07-13 11:34:05 -0300

    Atualização de arquivo principal

--- principal.py
+++ principal.py
@@ -1,6 +1,7 @@
 from controllers.menu import iniciar_menu_principal
 from datetime import datetime
 
-print(f"\n{datetime.now().strftime('%d/%m/%y, %H:%M:%S')}\n")
 
-iniciar_menu_principal()
+if __name__ == " main ":
+    print(f"\n{datetime.now().strftime('%d/%m/%y, %H:%M:%S')}\n")
+    iniciar_menu_principal()
